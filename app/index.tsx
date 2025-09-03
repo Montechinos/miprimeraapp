13202de26c63554c1a1383f01525f0283667a4dc
mateo.montesinos.j <mateo.montesinos.j@gmail.com>
2025-09-03 21:16:12 +0000

    Creacion de etiqueta para agrupar

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import { WelcomeText } from "@/components/WelcomeText";
 
 export default function Index() {
   return (
@@ -10,6 +11,7 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <WelcomeText></WelcomeText>
     </View>
   );
 }
