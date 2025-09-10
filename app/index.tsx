246cfc70f06eb58e3cd31c90630ebb6ff2eb7a9e
mateo.montesinos.j <mateo.montesinos.j@gmail.com>
2025-09-10 20:47:37 +0000

    🎨 style(WelcomeText): Cambios en apariencia - Edicion de estilos en WelcomeText - Modificacion de estilos en index

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,38 @@
-import { Text, View } from "react-native";
-import { WelcomeText } from "@/components/WelcomeText";
+import { StyleSheet,View, Text, ImageBackground } from 'react-native'
+import React from 'react'
+import WelcomeText from '@/components/WelcomeText'
 
-export default function Index() {
+const index = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <ImageBackground
+      source={require('@/IMG/image copy.png')} // Asegúrate de que la ruta sea correcta
+      style={styles.background}
+      resizeMode="cover"
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
       <WelcomeText></WelcomeText>
     </View>
-  );
+    </ImageBackground>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  text: {
+    color: 'black',
+    fontSize: 24,
+  },
+
+  background: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
+
+export default index
--- components/WelcomeText.tsx
+++ components/WelcomeText.tsx
@@ -1,7 +1,31 @@
-import { Text } from "react-native";
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
-export function WelcomeText():any {
-    return (<><Text>Hola Mundo</Text>
-            <Text>PEPE</Text></>
-    );
+const WelcomeText = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Bienvenido a mi App</Text>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+    maxHeight: 100,
+    maxWidth: 100,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+
+  text: {
+    color: 'black',
+    fontSize: 24,
+  },
+})
+
+export default WelcomeText
